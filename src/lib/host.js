c391a3b4f870165d9aa725f6aa61c449b7a9329f
ParkJoohyun <terajoohyun@ajou.ac.kr>
2021-02-16 17:42:25 +0900

    api server http to https

--- src/lib/host.js
+++ src/lib/host.js
@@ -1,6 +1,6 @@
 const host = 'http://localhost:3001'
-const destrib_host = 'http://terajoo.tk:3001'
+const destrib_host = 'https://terajoo.tk:3001'
 
 export const getHost = (n) => {
-    return host;
+    return destrib_host;
 }
